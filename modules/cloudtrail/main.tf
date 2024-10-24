966121f43f332b3577436bc8c9b0495b43f95d58
saravananganeshba <95737786+saravananganeshba@users.noreply.github.com>
2024-10-24 17:14:35 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -4,4 +4,7 @@ resource "aws_cloudtrail" "insecure-logging" {
   enable_logging = true
   enable_log_file_validation = true
   is_multi_region_trail = true
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail-logging.arn
+  kms_key_id = var.kms_key_arn
+  
 }
